bc481bfdee3f04d7624110e2a374b7d2d0f54335
14-05-12 <145626648+14-05-12@users.noreply.github.com>
2025-01-23 23:07:36 -0600

    Add files via upload

---
+++ Principal.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>ChapuStore</title>
+
+ 		<!-- Google font -->
+ 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+ 		<!-- Bootstrap -->
+ 		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+ 		<link rel="stylesheet" href=".whatsapp.css">
+
+ 		<!-- Slick -->
+ 		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+ 		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+ 		<!-- nouislider -->
+ 		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+ 		<!-- Font Awesome Icon -->
+ 		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+ 		<!-- Custom stlylesheet -->
+ 		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+<link rel="stylesheet" type="text/css" href="estilo_luan.css">
+
+    </head>
+	<center><body>
+
+		<a href="https://wa.me/7223229400?text=Estoy en ChapuStore-CECyTEM y quisiera tener atencion Personal" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
+
+			<!-- TOP HEADER -->
+
+			<center><div id="top-header" style="background: url(951.gif)">
+				<div class="container" style="background: url(951.gif)">
+
+					<ul class="header-links pull-left">
+						<li><a href="tel:+527224202599"><i class="fa fa-phone"></i> +52 722-420-2599</a></li>
+						<li><a href="#"><i class="fa fa-envelope-o"></i> luangfuentes1405@gmail.com</a></li>
+						<li><a href="#"><i class="fa fa-map-marker"></i>San Miguel Almoloyan, Almoloya de Juarez, México</a></li>
+						<li><a href="#"><i class="fa fa-phon"></i>C.P 5900</a></li>
+						
+					</ul>
+					
+			</div>
+	</form>
+							<!-- /LOGO -->
+
+						<!-- SEARCH BAR -->
+
+					<center><div class="cl-md-6">
+							<div class="header-search">
+									
+									
+									
+
+        <form action="buscar.php" method="post">
+            <center><img src="img/logo.jpeg" style=" width: 350px" height="350" align="center"><br>  
+            <br><input type="text" name="buscar" id="" class="input" placeholder="Auto a buscar">
+            <a><input type="submit" value="Buscar" class="search-btn"></a>
+           <br>
+           <br>
+        
+        
+            </form>
+    </div>
+</center></form></div>
+						<!-- /SEARCH BAR -->
+
+						<!-- ACCOUNT -->
+						
+								<!-- Menu Toogle -->
+								
+								<!-- /Menu Toogle -->
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+
+		<!-- NAVIGATION -->
+		<nav id="navigation">
+			<!-- container -->
+			<div class="container">
+				<!-- responsive-nav -->
+				<div id="responsive-nav">
+					<!-- NAV -->
+					<ul class="main-nav nav navbar-nav"> <li class="active"><a href="#">Home</a></li> <li></a></li>  
+					 <!-- /NAV -->
+				</div>
+				<!-- /responsive-nav -->
+			</div>
+			<!-- /container -->
+		</nav>
+		<!-- /NAVIGATION -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+								<img src="./img/f.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Modelos<br>FORD</h3>
+								<a href="index_ford.php" class="cta-btn">Mas <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+								<img src="./img/c.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Modelos<br>CHEVROLET</h3>
+								<a href="index.php" class="cta-btn">Mas <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+								<img src="./img/v.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Modelos<br>VOLKSVAGUEN</h3>
+								<a href="index_volksvaguen.php" class="cta-btn">Mas<i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+
+		<div id="hot-deal" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								
+									<center><img src="./img/6.jpeg" width="300"><br></center>
+							</ul>
+							<br>
+							<br><center>
+							<h2 class="text-uppercase">Aprovecha esta oportunidad</h2>
+							<p>Nuevo modelo</p>
+							<a href="http://localhost/PAGINAS_WEB_BD/CHAPUCAR/AppleiPhone11.html" class="primary-btn cta-btn" >Mas inf.</a>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /HOT DEAL SECTION -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+				
+										<!-- /product -->
+
+									</div></div>		<!-- FOOTER -->
+		<footer id="footer">
+			<!-- top footer -->
+			<div class="section">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">About Us</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+								<ul class="footer-links">
+									<li><a href="#"><i class="fa fa-map-marker"></i>Almoloya de juarez 50900</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>7223229400</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>luangfuentes1405@gmail.com</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">Categories</h3>
+								<ul class="footer-links">
+									<li><a href="#">Hot deals</a></li>
+									<li><a href="#">Laptops</a></li>
+									<li><a href="http://localhost/PAGINAS_WEB_BD/CHAPUSTORE/sstore.php">Smartphones</a></li>
+									<li><a href="#">Cameras</a></li>
+									<li><a href="#">Accessories</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="clearfix visible-xs"></div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">Information</h3>
+								<ul class="footer-links">
+									<li><a href="#">About Us</a></li>
+									<li><a href="#">Contact Us</a></li>
+									<li><a href="#">Privacy Policy</a></li>
+									<li><a href="#">Orders and Returns</a></li>
+									<li><a href="#">Terms & Conditions</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">Service</h3>
+								<ul class="footer-links">
+									<li><a href="#">My Account</a></li>
+									<li><a href="#">View Cart</a></li>
+									<li><a href="#">Wishlist</a></li>
+									<li><a href="#">Track My Order</a></li>
+									<li><a href="#">Help</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /top footer -->
+
+			<!-- bottom footer -->
+			<div id="bottom-footer" class="section">
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<ul class="footer-payments">
+								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
+								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+							</ul>
+							<span class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</span>
+						</div>
+					</div>
+						<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /bottom footer -->
+		</footer>
+		<!-- /FOOTER -->
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
+
